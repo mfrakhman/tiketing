dde341a652918bf526dcf37b491e27d458fdd375
mfrakhman <rrrfajar1@gmail.com>
2026-07-24 17:37:35 +0700

    init

---
+++ src/main/java/com/etick/tiketing/TiketingApplication.java
@@ -0,0 +1,13 @@
+package com.etick.tiketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiketingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/etick/tiketing/TiketingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etick.tiketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiketingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
